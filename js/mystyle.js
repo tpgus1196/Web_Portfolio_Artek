509eb8ee06c81ae1220cd293c88cc090db637e42
tpgus1196 <tpgus1196@naver.com>
2023-06-13 20:19:43 +0900

    아르텍 웹페이지 리디자인

---
+++ js/mystyle.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   
+  
+      
+   var wh = $(document).height();
+   var ww = $(document).width();
+   
+ 
+   $(".wh").height(wh);
+   $(".ww").height(ww);
+      
+   
+})
